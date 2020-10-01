6a9b57a517c63425780e8fc207fc230279475d74
Ryan <ryanjonesr@gmail.com>
2020-10-01 09:14:00 +0100

    added feature file, enabled browserstack capability

--- cypress/integration/Footer/footer.js
+++ cypress/integration/Footer/footer.js
@@ -2,6 +2,10 @@ import {FooterPage} from "../../page-objects/footerPage"
 
 const footerPage = new FooterPage()
 
+import { Given } from "cypress-cucumber-preprocessor/steps";
+import { When } from "cypress-cucumber-preprocessor/steps";
+import { Then } from "cypress-cucumber-preprocessor/steps";
+
 
 When("I Open the Intl Homepage for {string}", function (content) {
     footerPage.navigateToHomePage(content)
